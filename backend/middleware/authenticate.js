06b9d750aa264c7371e9d8b47d1918ba42884cdf
MohamedCoderX <mdibrahim.developer@gmail.com>
2025-03-26 12:45:36 +0530

    loki

--- backend/middleware/authenticate.js
+++ backend/middleware/authenticate.js
@@ -2,17 +2,25 @@ const ErrorHanlder = require("../utils/errorHandler");
 const catchAsyncError = require("./catchAsyncError");
 const jwt = require('jsonwebtoken')
 const User = require('../models/userModel')
-exports.isAunthenticatedUser = catchAsyncError(async(req,res,next)=>{
- const {token} = req.cookies;
- 
- if(!token){
-    return next(new ErrorHanlder('Login first to handled to this resource',401))
- }
 
- const decoded = jwt.verify(token,process.env.JWT_SECRET)
-  req.user = await User.findById(decoded.id)
-  next();
-})
+exports.isAunthenticatedUser = catchAsyncError(async (req, res, next) => {
+   console.log("Cookies Received:", req.cookies);  // Debugging
+   console.log("Authorization Header:", req.headers.authorization);
+
+   const { token } = req.cookies;
+
+   if (!token) {
+       return next(new ErrorHanlder('Login first to handle this resource', 401));
+   }
+
+   try {
+       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+       req.user = await User.findById(decoded.id);
+       next();
+   } catch (error) {
+       return next(new ErrorHanlder("Invalid token", 401));
+   }
+});
 
 exports.authorizeRoles = (...roles) =>{
    return(req,res,next) => {
