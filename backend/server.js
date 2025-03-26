f0644c01400ec2f9b4047b4363adcb60439bebb9
MohamedCoderX <mdibrahim.developer@gmail.com>
2025-03-26 15:05:40 +0530

    bakcend

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,13 @@ const connectDatabase = require('./config/database');
 
 connectDatabase();
 
-const server = app.listen(process.env.PORT || 5000, "0.0.0.0", () => {
-    console.log(`Server running on port ${process.env.PORT || 5000} in ${process.env.NODE_ENV}`);
-});
-
 app.get("/", (req, res) => {
     res.json({ message: "API is running successfully!" });
   });
+
+const server = app.listen(process.env.PORT || 5000, "0.0.0.0", () => {
+    console.log(`Server running on port ${process.env.PORT || 5000} in ${process.env.NODE_ENV}`);
+});
 
 process.on('unhandledRejection',(err)=>{
     console.log(`Error: ${err.message}`);
@@ -19,11 +19,11 @@ process.on('unhandledRejection',(err)=>{
     })
 })
 
-process.on('uncaughtException',(err)=>{
-    console.log(`Error: ${err.message}`);
-    console.log('Shutting down the server due to uncaught exception error');
-    server.close(()=>{
-        process.exit(1);
-    })
-})
+// process.on('uncaughtException',(err)=>{
+//     console.log(`Error: ${err.message}`);
+//     console.log('Shutting down the server due to uncaught exception error');
+//     server.close(()=>{
+//         process.exit(1);
+//     })
+// })
 
