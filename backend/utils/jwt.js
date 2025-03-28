95f7d35e8a472db06d5eaf1ca211b15cce9990f2
MohamedCoderX <mdibrahim.developer@gmail.com>
2025-03-28 23:37:10 +0530

    cookeis

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -8,6 +8,8 @@ const sendToken = (user,statusCode,res)=>{
         expires:new Date(Date.now() + process.env.COOKIE_EXPIRES_TIME *24 *60 *60 *1000
     ),
     httpOnly:true,
+    secure: true,   // Required for HTTPS
+    sameSite: "None", // Allows cross-site cookies
     }
 
 
