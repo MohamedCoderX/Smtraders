9e8f54349d36475fbcd63193b3c47580cd94bac3
MohamedCoderX <mdibrahim.developer@gmail.com>
2025-04-03 23:23:51 +0530

    sas

--- frontend/src/components/admin/ProductList.jsx
+++ frontend/src/components/admin/ProductList.jsx
@@ -1,122 +1,116 @@
-import { Fragment, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { toast } from "react-toastify";
-import { clearError } from "../../slices/authslice";
-import Loader from "../Loader";
-import { deleteProduct, getAdminProducts } from "../../actions/productActions";
-import { MDBDataTable } from "mdbreact";
-import { Button, Card, Container, Row, Col } from "react-bootstrap";
-import Sidebar from "./Sidebar";
-import { clearProductDeleted } from "../../slices/productSlice";
+import { Fragment, useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { toast } from "react-toastify"
+import { clearError } from "../../slices/authslice"
+import Loader from "../Loader"
+import { deleteProduct, getAdminProducts } from "../../actions/productActions"
+import {MDBDataTable} from 'mdbreact'
+import { Link } from "react-router-dom"
+import { Button, NavDropdown } from "react-bootstrap"
+import Sidebar from "./Sidebar"
+import { clearProductDeleted } from "../../slices/productSlice"
 
-export default function ProductList() {
-  const { products = [], loading = true, error } = useSelector((state) => state.productsState);
-  const { isProductDeleted, error: productError } = useSelector((state) => state.productState);
-  const dispatch = useDispatch();
-
-  const deleteHandler = (e, id) => {
-    e.target.disabled = true;
-    dispatch(deleteProduct(id));
-  };
-
-  const setProducts = () => {
-    const data = {
-      columns: [
-        {
-          label: "Product",
-          field: "product",
-          sort: "asc",
-        },
-        {
-          label: "Price",
-          field: "price",
-          sort: "asc",
-        },
-        {
-          label: "Stock",
-          field: "stock",
-          sort: "asc",
-        },
-        {
-          label: "Actions",
-          field: "actions",
-          sort: "asc",
-        },
-      ],
-      rows: [],
-    };
-
-    products.forEach((product) => {
-      data.rows.push({
-        product: (
-          <div className="d-flex align-items-center">
-            <img
-              src={product.image}
-              alt={product.name}
-              className="rounded img-thumbnail me-3"
-              style={{ width: "50px", height: "50px", objectFit: "cover" }}
-            />
-            <span className="fw-bold">{product.name}</span>
-          </div>
-        ),
-        price: <span className="text-success fw-bold">₹{product.price}</span>,
-        stock: <span className="fw-medium">{product.stock}</span>,
-        actions: (
-          <Fragment>
-            <Button variant="danger" className="py-1 px-2 ms-2" onClick={(e) => deleteHandler(e, product._id)}>
-              <i className="fa fa-trash"></i>
-            </Button>
-          </Fragment>
-        ),
-      });
-    });
-
-    return data;
-  };
-
-  useEffect(() => {
-    if (error || productError) {
-      toast(error || productError, {
-        type: "error",
-        onOpen: () => {
-          dispatch(clearError());
-        },
-      });
-      return;
+export default function ProductList(){
+    const {products = [],loading = true ,error} = useSelector(state=>state.productsState)
+    const {isProductDeleted,error:productError} = useSelector(state=>state.productState)
+    const dispatch = useDispatch();
+    // const deleteHandler = (e, id) => {
+    //     e.target.disabled = true;
+    //     dispatch(deletePr(id))
+    // }
+    const deleteHandler = (e, id) => {
+        e.target.disabled = true;
+        dispatch(deleteProduct(id))
     }
-    if (isProductDeleted) {
-      toast("Product Deleted Successfully!", {
-        type: "success",
-        onOpen: () => dispatch(clearProductDeleted()),
-      });
-      return;
+    const setProducts = () => {
+        const data = {
+            columns : [
+                {
+                    label: 'ID',
+                    field: 'id',
+                    sort: 'asc'
+                },
+                {
+                    label: 'Name',
+                    field: 'name',
+                    sort: 'asc'
+                },
+                {
+                    label: 'Price',
+                    field: 'price',
+                    sort: 'asc'
+                },
+                {
+                    label: 'Stock',
+                    field: 'stock',
+                    sort: 'asc'
+                },
+                {
+                    label: 'Actions',
+                    field: 'actions',
+                    sort: 'asc'
+                }
+            ],
+            rows : []
+        }
+
+        products.forEach( product => {
+            data.rows.push({
+                id: product._id,
+                name: product.name,
+                price : `₹${product.price}`,
+                stock: product.stock,
+                actions: (
+                    <Fragment>
+                       
+                        <Button onClick={e => deleteHandler(e, product._id)}  className="btn btn-danger py-1 px-2 ml-2">
+                            <i className="fa fa-trash"></i>
+                        </Button>
+                    </Fragment>
+                )
+            })
+        })
+
+        return data;
     }
 
-    dispatch(getAdminProducts);
-  }, [dispatch, error, isProductDeleted]);
+    useEffect(() => {
+        if(error || productError) {
+            toast(error || productError, {
+                type: 'error',
+                onOpen: ()=> { dispatch(clearError()) }
+            })
+            return
+        }
+        if(isProductDeleted) {
+            toast('Product Deleted Succesfully!',{
+                type: 'success',
+                onOpen: () => dispatch(clearProductDeleted())
+            })
+            return;
+        }
 
-  return (
-    <Container fluid>
-      <Row>
-        {/* Sidebar */}
-        <Col md={2} className="bg-light vh-100">
-          <Sidebar />
-        </Col>
-
-        {/* Product List Content */}
-        <Col md={10}>
-          <Card className="my-4 p-4 shadow-sm">
-            <Card.Title className="text-center text-primary fw-bold fs-3">Product List</Card.Title>
-            <Card.Body>
-              {loading ? (
-                <Loader />
-              ) : (
-                <MDBDataTable data={setProducts()} bordered striped hover className="px-3" />
-              )}
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
-  );
+        dispatch(getAdminProducts)
+    },[dispatch, error, isProductDeleted])
+    return(
+        <div className="row">
+        <div className="col-12 col-md-2">
+        <Sidebar/>
+        </div>
+        <div className="col-12 col-md-10">
+            <h1 className="my-4">Product List</h1>
+            <Fragment>
+                {loading ? <Loader/> : 
+                    <MDBDataTable
+                        data={setProducts()}
+                        bordered
+                        striped
+                        hover
+                        className="px-3"
+                    />
+                }
+            </Fragment>
+        </div>
+    </div>
+    )
 }
