606fa8ecb1ef8ab34b03b0967cf8c41f53773ea9
MohamedCoderX <mdibrahim.developer@gmail.com>
2025-09-10 21:14:52 +0530

    asdas

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -6,9 +6,7 @@ const Header = () => {
   return (
    <div className="header">
     <div className="header-content">
-        <h1>Celebrate Diwali with <br /><span>SM CRACKERS! 90% OFFER</span></h1>
-        
-        <p>This Diwali, elevate your celebrations with our wide range of vibrant and exciting crackers! 🎆 Whether you're looking for classic sparklers, stunning flower pots, or breathtaking aerial shots, we have everything you need to make your festivities unforgettable.</p>
+       
         <Link to={'/products'}>
         <button >View menu</button>
         </Link>
