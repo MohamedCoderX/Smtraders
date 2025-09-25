0d96015bf66be532833088b2cba93a176edb8a42
MohamedCoderX <mdibrahim.developer@gmail.com>
2025-09-25 12:35:44 +0530

    liks

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,90 +1,57 @@
-import React from "react";
-import "./Footer.css";
-import { useNavigate } from "react-router-dom";
+import React from 'react'
+import './Footer.css'
+import { useNavigate } from 'react-router-dom';
 
 const Footer = () => {
-  const navigate = useNavigate();
-
+    const navigate = useNavigate();
   return (
     <div>
-      {/* Footer Main */}
-      <div className="footer-full">
-        {/* Logo + Menu */}
-        <div className="logoss">
-          <div className="logosimg">
-            <img
-              src="../images/logo.png"
-              alt="SM Crackers logo"
-              loading="lazy"
-            />
-          </div>
-
-          <ul className="menu">
-            <li onClick={() => navigate("/")}>Home</li>
-            <li onClick={() => navigate("/products")}>Products</li>
-            <li onClick={() => navigate("/about")}>About Us</li>
-            <li onClick={() => navigate("/contact")}>Contact Us</li>
-            <li onClick={() => navigate("/terms")}>Terms & Conditions</li>
-            <li onClick={() => navigate("/privacy")}>Privacy Policy</li>
+    <div className="footer-full">
+ <div className="logoss">
+         <div className='logosimg'>
+         <img src="../images/logo.png" alt="SM Crackers logo" loading='lazy'/>
+         </div>
+            <ul className="menu">
+            <li onClick={()=>navigate('')}>Home</li>
+         | <li onClick={()=>navigate('/products')}>Products</li> |
+          <li onClick={()=>navigate('/About')}>About-us</li> |
+          <li onClick={()=>navigate('/contact')} > Contact-us</li>|
+          <li onClick={()=>navigate('/terms')} > Terms and conditions</li>
           </ul>
-
-          <h3>Serving Customers Since 2015</h3>
+            <h3>SM Crackers @2015</h3>
         </div>
-
-        {/* Contact Info */}
         <div className="contact">
-          <h2>Contact Us</h2>
-          <div className="contact_s1">
-            <a href="#">
-              <i className="fa fa-map-marker" aria-hidden="true"></i>
-            </a>
-            <p>
-              4/175/A Sattur to Sivakasi Road <br />
-              Veerapandiyapuram <br />
-              Near Toll Gate, Sattur - 626203
-            </p>
-          </div>
-
-          <div className="contact_s1">
-            <a href="tel:+918903359989">
-              <i className="fa fa-mobile" aria-hidden="true"></i>
-            </a>
-            <p>+91 8903359989</p>
-          </div>
-
-          <div className="contact_s1">
-            <a href="mailto:smpyropark.2019@gmail.com">
-              <i className="fa fa-envelope-o" aria-hidden="true"></i>
-            </a>
-            <p>smpyropark.2019@gmail.com</p>
-          </div>
+        <h2>Contact us</h2>
+            <div className="contact_s1">
+                <a href="#"><i class="fa fa-map-marker" aria-hidden="true"></i></a>
+                <p>4/175/A Sattur to Sivakasi road
+                    <br /> Veerapandiyapuram 
+                    <br />
+                    Near by toll gate Sattur - 626203</p>
+            </div>
+            <div className="contact_s1">
+                <a href="#"><i class="fa fa-mobile" aria-hidden="true"></i></a>
+                <p>+91 8903359989</p>
+            </div>
+            <div className="contact_s1">
+                <a href="#"><i class="fa fa-envelope-o" aria-hidden="true"></i></a>
+                <p>smpyropark.2019@gmail.com</p>
+            </div>
         </div>
-
-        {/* About Company */}
         <div className="abtcom">
-          <h2>About the Company</h2>
-          <p>
-            At SM Crackers, customer satisfaction and safety are our top
-            priorities. Our online store lets you browse and order crackers
-            securely, with easy payments and reliable delivery.
-          </p>
-
-          <div className="about_icon">
-            <a href="https://www.instagram.com/sm_crackers_sivakasi/">
-              <i className="fa fa-facebook"></i>
-            </a>
-            
-          </div>
+            <h2>About the company</h2>
+            <p>At SM Crackers, customer satisfaction and safety are our top priorities. Our user-friendly online store allows you to browse, choose, and order crackers with ease, all while ensuring secure payments and reliable delivery.
+            </p>
+               
         </div>
-      </div>
+<hr />
 
-      {/* Copyright */}
-      <div className="copyright">
-        &copy; {new Date().getFullYear()} SM Crackers. All Rights Reserved. |{" "}
-        <span>&hearts;</span> Made with Love in Sattur
-      </div>
     </div>
-  );
-};
+    <div className="copyright">
+    &copy; Copyright 2024 | All Rights Reserved SM Crackers,Sattur , Made with <span className='her' >&hearts;</span>
+</div>
+</div>
+  )
+}
 
-export default Footer;
+export default Footer
