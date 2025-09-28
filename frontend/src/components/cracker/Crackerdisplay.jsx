92e648deb12df6baadafe12f32e2499615cc6a10
MohamedCoderX <mdibrahim.developer@gmail.com>
2025-09-28 17:12:21 +0530

    commited

--- frontend/src/components/cracker/Crackerdisplay.jsx
+++ frontend/src/components/cracker/Crackerdisplay.jsx
@@ -3,61 +3,58 @@ import "./cracker.css";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { getProducts } from "../../actions/productActions";
-import FlashingOfferBox from "../../components/FlashingOfferBox";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Product from "../../Pages/products/Product";
 
+// Skeleton Loader for Product Cards
+const ProductSkeleton = () => {
+  return (
+    <div className="skeleton-card">
+      <div className="skeleton-img" />
+      <div className="skeleton-text" />
+      <div className="skeleton-text short" />
+    </div>
+  );
+};
+
 const Crackerdisplay = () => {
   const dispatch = useDispatch();
-  const { products,error } = useSelector(
+  const navigate = useNavigate();
+
+  const { products, error, loading } = useSelector(
     (state) => state.productsState
   );
+
+  // Fetch products on mount
   useEffect(() => {
     if (error) {
       return toast.error(error);
     }
     dispatch(getProducts(null));
-  }, [dispatch,error]);
-
-  const navigate = useNavigate();
+  }, [dispatch, error]);
 
   return (
     <div>
-      <div className="cracker-display" id="cracker-diplay">
+      <div className="cracker-display" id="cracker-display">
         <h2>All Type Of Crackers</h2>
-        
+
         <Fragment>
-            <div className="cracker-display-list">
-                {products && products.map(product => (
-                    <Product key={product._id} product={product}/>
-                    //<div className="cracker-list-item" key={product._id}>
-                //     <div className="cracker-img">
-                //         <img src={product.images[0].image} alt="img" />
-                        
-                //     </div>
-                //     <div className="cracker-item-info">
-                //         <div className="cracker-review">
-                //             <p>{product.name}</p>
+          <div className="cracker-display-list">
+            {loading ? (
+              // Show skeletons when loading
+              Array.from({ length: 8 }).map((_, index) => (
+                <ProductSkeleton key={index} />
+              ))
+            ) : (
+              products &&
+              products.map((product) => (
+                <Product key={product._id} product={product} />
+              ))
+            )}
+          </div>
+        </Fragment>
 
-                //         </div>
-                //         <p className="cracker-iem-desc">
-
-                //             {product.description}
-                //         </p>
-                //         <p className="cracker-item-price">
-                //             {product.price}
-                //         </p>
-                //        <div className="cart">
-                //     <div className="add">
-                //     <button> <i className="fa-solid fa-cart-shopping"></i> Add To Cart</button>
-                //     </div>
-                //        </div>
-                //     </div>
-                //
-                ))}
-            </div>
-            </Fragment>
         <div className="view-all">
           <button onClick={() => navigate("/products")}>
             View All Crackers
